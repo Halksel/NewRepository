1efe24a14ec917f85646c639252065ebdb042478
Halksel <whentheycry0708@gmail.com>
2019-04-16 15:12:31 +0900

    signal 考察3

--- C/signal.c
+++ C/signal.c
@@ -6,7 +6,7 @@ void (*func)(int);
 void (*signal(int sig, void (*func(int))))(int hoge);
 // 関数の部分適用？
 // int と intを受け取りvoidを返す関数へのポインタを受け取る関数にintを部分適用した戻り値がvoid
-// int と intを受け取りvoidを返す関数へのポインタがintを受け取り、voidを返すsignalという関数である
+// int と intを受け取りvoidを返す関数へのポインタがintを受け取り、voidを返すsignalという関数へのポインターである
 void (*signal(Args))(int)
 int main(){
   printf("%p\n",func); 
