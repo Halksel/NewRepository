5775b2abc035004b83c268a537e2cd785b312824
Halksel <whentheycry0708@gmail.com>
2019-04-16 11:59:18 +0900

    signal

---
+++ C/signal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+//Args
+int sig;
+void (*func)(int);
+void (*signal(int sig, void (*func(int))))(int);
+// 関数の部分適用？
+// int と intを受け取りvoidを返す関数へのポインタを受け取る関数にintを部分適用した戻り値がvoid
+int main(){
+  printf("%p\n",func); 
+  printf("%p\n",signal); 
+}
