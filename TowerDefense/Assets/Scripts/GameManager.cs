e82f51571dc9a9a8f8102b96f305f0bd951c2ec4
Halksel <whentheycry0708@gmail.com>
2019-04-19 16:49:34 +0900

    add GameManager

--- TowerDefense/Assets/Scripts/GameManager.cs
+++ TowerDefense/Assets/Scripts/GameManager.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+using Road = System.Collections.Generic.List<UnityEngine.GameObject>;
 
+public class GameManager : MonoBehaviour {
+	public struct EnemySpwanInfo
+	{
+			public int roadId;
+			public int time;
+			public int enemyId;
+	}
+	public Road road;
+	public List<Road> roads;
+
+	public int time;
+	public List<EnemySpwanInfo> enemySpawninfo;
+
+	[SerializeField]
+	private TowerManager towerManager;
+	[SerializeField]
+	private EnemyManager enemyManager;
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+	}
+
+	public void GameOver(){
+
 	}
 }
