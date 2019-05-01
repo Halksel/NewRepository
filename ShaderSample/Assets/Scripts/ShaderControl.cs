b08b8f3b1b1d73e36959d88b5e8c14712d508e41
Halksel <whentheycry0708@gmail.com>
2019-05-01 17:06:35 +0900

    Add random shader

---
+++ ShaderSample/Assets/Scripts/ShaderControl.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShaderControl : MonoBehaviour {
+	void Start () {
+		GetComponent<Renderer> ().material.SetColor ("_BaseColor", Color.black);
+	}
+}
