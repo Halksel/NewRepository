5c0e6f94b3e713bc26f5b7f15fb5faf614204663
Halksel <whentheycry0708@gmail.com>
2019-05-04 19:34:20 +0900

    wipe

---
+++ ShaderSample/Assets/Scripts/PostEffect.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PostEffect : MonoBehaviour {
+
+	public Material wipeCircle;
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		Graphics.Blit (src, dest, wipeCircle);
+	}
+}
