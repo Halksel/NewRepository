4b406776d6b901eba58af4b038e9fd4e28453352
Halksel <whentheycry0708@gmail.com>
2019-05-04 18:14:35 +0900

    add Phong

---
+++ ShaderSample/Assets/Scripts/DispDepth.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DispDepth : MonoBehaviour {
+    public Material mat;
+    void Start () 
+    {
+        GetComponent<Camera>().depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+    public void OnRenderImage(RenderTexture source, RenderTexture dest)
+    {                
+        Graphics.Blit(source, dest, mat);
+    }
+}
